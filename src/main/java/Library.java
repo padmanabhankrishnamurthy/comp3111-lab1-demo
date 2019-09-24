8f7dc8271aee315f4cfc252e7a0b8315d6baf92c
padmanabhankrishnamurthy <pkaa@connect.ust.hk>
2019-09-24 15:02:57 +0800

    Initial Project Setup

---
+++ src/main/java/Library.java
@@ -0,0 +1,10 @@
+public class Library {
+    public static void main(String arg[])
+    {
+        System.out.println("When there is fire, commit and push");
+    }
+    public boolean someLibraryMethod()
+    {
+        return true;
+    }
+}
